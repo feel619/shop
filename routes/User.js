a055fc8010e33488d02f75ee20b26d65db749178
FeelPatel <final@aether.co.in>
2024-02-28 21:22:38 +0530

    user route

--- routes/User.js
+++ routes/User.js
@@ -9,57 +9,8 @@ const moment = require('moment-timezone');
 moment.tz.setDefault("Asia/Kolkata");
 const config = require("config");
 const jwt = require("jsonwebtoken");
-//const { db } = require("../models");
-//const Op = db.Sequelize.Op;
-//const EmployeeMst = require('../viewModel/EmployeeMst');
-//const { } = require("../controllers/AstAllocation");
 const { ShopUsers } = require('../models/ShopUsers');
 require('dotenv').config();
-const { body, checkSchema, validationResult } = require('express-validator');
-
-const registrationSchema = {
-  name: {
-    custom: {
-      options: async value => {
-        return ShopUsers.find({
-          Name: value
-        }).then(user => {
-          if (user.length > 0) {
-            return Promise.reject('Username already in use')
-          }
-        })
-      }
-    }
-  },
-  password: {
-    isStrongPassword: {
-      minLength: 8,
-      minLowercase: 1,
-      minUppercase: 1,
-      minNumbers: 1
-    },
-    errorMessage: "Password must be greater than 8 and contain at least one uppercase letter, one lowercase letter, and one number",
-  },
-  phone: {
-    notEmpty: true,
-    errorMessage: "Phone number cannot be empty"
-  },
-  email: {
-    normalizeEmail: true,
-    custom: {
-      options: async value => {
-        return ShopUsers.find({
-          email: value
-        }).then(user => {
-          if (user.length > 0) {
-            return Promise.reject('Email address already taken')
-          }
-        })
-      }
-    }
-  }
-}
-
 
 function validateRegister(users) {
   winston.log("info", ` Customer validateRegister  ${JSON.stringify(users)}  `);
@@ -80,13 +31,6 @@ router.post("/register", async (req, res) => {
     winston.log("error", `user route register ${JSON.stringify(error)}`);
     return res.status(400).send({ "status": true, "message": error.details[0].message });
   }
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   const alert = errors.array()
-  //   res.redirect('login', {
-  //     alert
-  //   })
-  // }
   const isExist = await ShopUsers.findOne({ Email: reqBody?.email });
   if (isExist) {
     //return Promise.reject('Email address already taken')
